4b8df9fd0067da69d9aa216565e488aa5312c98e
AndreVTavares <andretavares3@gmail.com>
2021-02-06 16:08:00 -0300

    implementing appointment created page and navegate to dashboard

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,6 +1,39 @@
-import React from 'react';
-import { View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
 
-const AppointmentCreated: React.FC = () => <View />;
+import {
+  Container,
+  Title,
+  Description,
+  OkButton,
+  OkButtonText,
+} from './styles';
+
+const AppointmentCreated: React.FC = () => {
+  const { reset } = useNavigation();
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [{ name: 'Dashboard' }],
+      index: 0,
+    });
+  }, [reset]);
+
+  return (
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento concluido</Title>
+      <Description>
+        Terça, dia 06 de março de 2020 as 16:00h com André Tavares
+      </Description>
+
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
+    </Container>
+  );
+};
 
 export default AppointmentCreated;
